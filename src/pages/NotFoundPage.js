a9317bfd78472e17d68fd31ff733bcd44a0ccbc9
hassam4817 <hassam4817@gmail.com>
2022-01-14 11:36:25 +0000

    initial not found page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1 className="text-center my-5">404 Page not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
