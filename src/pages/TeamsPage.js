a937c9244668c97f566e750ee6accf866dcfd12b
hassam4817 <hassam4817@gmail.com>
2022-01-14 11:36:57 +0000

    split the content into components

--- src/pages/TeamsPage.js
+++ src/pages/TeamsPage.js
@@ -1,81 +1,26 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import TeamsCreateNew from "../components/TeamsCreateNew";
+import TeamsList from "../components/TeamsList";
 
 const TeamsPage = () => {
   const [teams, setTeams] = useState([""]);
-  const [newTeam, setNewTeam] = useState("");
-  const [success, setSuccess] = useState("");
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const getTeams = async () => {
-      try {
-        const response = await axios.get(
-          "http://stubber.test.visiblethread.com/teams/allNames"
-        );
-
-        // console.log(response.data);
-        setTeams(response.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    getTeams();
-  }, []);
-
-  const createTeam = async (team) => {
-    try {
-      const response = await axios.post(
-        "http://stubber.test.visiblethread.com/teams/add",
-        { teamName: team }
-      );
-      // console.log(response.data);
-      setNewTeam("");
-
-      setSuccess("Team has been succesfully created");
-      setTimeout(() => setSuccess(""), 2000);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const checkIfTeamExists = (team) => {
-    if (teams.indexOf(team) >= 0) return true;
-    else return false;
-  };
-
-  const userFormSubmit = (e) => {
-    e.preventDefault();
-    if (checkIfTeamExists(newTeam)) {
-      setError("This user already exists");
-      setTimeout(() => setError(""), 2000);
-    } else {
-      createTeam(newTeam);
-    }
-  };
 
   return (
-    <div>
-      <h2>These are all the teams available in your organisation</h2>
-      <ul>
-        {teams.map((team, index) => (
-          <li key={index}>{team}</li>
-        ))}
-      </ul>
-      <form onSubmit={userFormSubmit}>
-        <input
-          type="text"
-          placeholder="Team name"
-          value={newTeam}
-          onChange={(e) => setNewTeam(e.target.value)}
-          required
-        />
-        <button>Create</button>
-      </form>
-      {success === "" || <p>{success}</p>}
-      {error === "" || <p>{error}</p>}
-    </div>
+    <>
+      <div className="py-5">
+        <h2 className="text-center">Teams available in your organisation</h2>
+
+        <Row className=" my-5">
+          <Col lg={4} xl={3} className="mb-5">
+            <TeamsCreateNew teams={teams} setTeams={setTeams} />
+          </Col>
+          <Col lg={{ span: 8, order: "first" }} xl={9}>
+            <TeamsList teams={teams} setTeams={setTeams} />
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
