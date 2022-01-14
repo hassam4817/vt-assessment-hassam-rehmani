d04ae74e912173d08f7a8dea9eb1d05961f4f108
hassam4817 <hassam4817@gmail.com>
2022-01-14 11:35:52 +0000

    reports dummy data to avoid calling api in dev

---
+++ src/data/dummy_data.js
@@ -0,0 +1,364 @@
+const DUMMY_DATA = [
+  {
+    teamName: "Team 1",
+    totalScans: 1587,
+    reports: [
+      {
+        teamName: "Team 1",
+        date: "2021-12-13",
+        scansAMonth: 256,
+      },
+      {
+        teamName: "Team 1",
+        date: "2021-11-13",
+        scansAMonth: 124,
+      },
+      {
+        teamName: "Team 1",
+        date: "2021-10-13",
+        scansAMonth: 349,
+      },
+      {
+        teamName: "Team 1",
+        date: "2021-09-13",
+        scansAMonth: 441,
+      },
+      {
+        teamName: "Team 1",
+        date: "2021-08-13",
+        scansAMonth: 407,
+      },
+      {
+        teamName: "Team 1",
+        date: "2021-07-13",
+        scansAMonth: 10,
+      },
+    ],
+  },
+  {
+    teamName: "Team 2",
+    totalScans: 2884,
+    reports: [
+      {
+        teamName: "Team 2",
+        date: "2021-12-13",
+        scansAMonth: 253,
+      },
+      {
+        teamName: "Team 2",
+        date: "2021-11-13",
+        scansAMonth: 737,
+      },
+      {
+        teamName: "Team 2",
+        date: "2021-10-13",
+        scansAMonth: 432,
+      },
+      {
+        teamName: "Team 2",
+        date: "2021-09-13",
+        scansAMonth: 932,
+      },
+      {
+        teamName: "Team 2",
+        date: "2021-08-13",
+        scansAMonth: 297,
+      },
+      {
+        teamName: "Team 2",
+        date: "2021-07-13",
+        scansAMonth: 233,
+      },
+    ],
+  },
+  {
+    teamName: "Team 3",
+    totalScans: 2184,
+    reports: [
+      {
+        teamName: "Team 3",
+        date: "2021-12-13",
+        scansAMonth: 970,
+      },
+      {
+        teamName: "Team 3",
+        date: "2021-11-13",
+        scansAMonth: 290,
+      },
+      {
+        teamName: "Team 3",
+        date: "2021-10-13",
+        scansAMonth: 139,
+      },
+      {
+        teamName: "Team 3",
+        date: "2021-09-13",
+        scansAMonth: 239,
+      },
+      {
+        teamName: "Team 3",
+        date: "2021-08-13",
+        scansAMonth: 343,
+      },
+      {
+        teamName: "Team 3",
+        date: "2021-07-13",
+        scansAMonth: 203,
+      },
+    ],
+  },
+  {
+    teamName: "Team 4",
+    totalScans: 3304,
+    reports: [
+      {
+        teamName: "Team 4",
+        date: "2021-12-13",
+        scansAMonth: 519,
+      },
+      {
+        teamName: "Team 4",
+        date: "2021-11-13",
+        scansAMonth: 692,
+      },
+      {
+        teamName: "Team 4",
+        date: "2021-10-13",
+        scansAMonth: 208,
+      },
+      {
+        teamName: "Team 4",
+        date: "2021-09-13",
+        scansAMonth: 819,
+      },
+      {
+        teamName: "Team 4",
+        date: "2021-08-13",
+        scansAMonth: 519,
+      },
+      {
+        teamName: "Team 4",
+        date: "2021-07-13",
+        scansAMonth: 547,
+      },
+    ],
+  },
+  {
+    teamName: "Team 5",
+    totalScans: 3160,
+    reports: [
+      {
+        teamName: "Team 5",
+        date: "2021-12-13",
+        scansAMonth: 897,
+      },
+      {
+        teamName: "Team 5",
+        date: "2021-11-13",
+        scansAMonth: 568,
+      },
+      {
+        teamName: "Team 5",
+        date: "2021-10-13",
+        scansAMonth: 118,
+      },
+      {
+        teamName: "Team 5",
+        date: "2021-09-13",
+        scansAMonth: 722,
+      },
+      {
+        teamName: "Team 5",
+        date: "2021-08-13",
+        scansAMonth: 242,
+      },
+      {
+        teamName: "Team 5",
+        date: "2021-07-13",
+        scansAMonth: 613,
+      },
+    ],
+  },
+  {
+    teamName: "Team 6",
+    totalScans: 4299,
+    reports: [
+      {
+        teamName: "Team 6",
+        date: "2021-12-13",
+        scansAMonth: 740,
+      },
+      {
+        teamName: "Team 6",
+        date: "2021-11-13",
+        scansAMonth: 788,
+      },
+      {
+        teamName: "Team 6",
+        date: "2021-10-13",
+        scansAMonth: 279,
+      },
+      {
+        teamName: "Team 6",
+        date: "2021-09-13",
+        scansAMonth: 755,
+      },
+      {
+        teamName: "Team 6",
+        date: "2021-08-13",
+        scansAMonth: 793,
+      },
+      {
+        teamName: "Team 6",
+        date: "2021-07-13",
+        scansAMonth: 944,
+      },
+    ],
+  },
+  {
+    teamName: "Team 7",
+    totalScans: 4361,
+    reports: [
+      {
+        teamName: "Team 7",
+        date: "2021-12-13",
+        scansAMonth: 508,
+      },
+      {
+        teamName: "Team 7",
+        date: "2021-11-13",
+        scansAMonth: 535,
+      },
+      {
+        teamName: "Team 7",
+        date: "2021-10-13",
+        scansAMonth: 658,
+      },
+      {
+        teamName: "Team 7",
+        date: "2021-09-13",
+        scansAMonth: 999,
+      },
+      {
+        teamName: "Team 7",
+        date: "2021-08-13",
+        scansAMonth: 812,
+      },
+      {
+        teamName: "Team 7",
+        date: "2021-07-13",
+        scansAMonth: 849,
+      },
+    ],
+  },
+  {
+    teamName: "Team 8",
+    totalScans: 3741,
+    reports: [
+      {
+        teamName: "Team 8",
+        date: "2021-12-13",
+        scansAMonth: 446,
+      },
+      {
+        teamName: "Team 8",
+        date: "2021-11-13",
+        scansAMonth: 743,
+      },
+      {
+        teamName: "Team 8",
+        date: "2021-10-13",
+        scansAMonth: 738,
+      },
+      {
+        teamName: "Team 8",
+        date: "2021-09-13",
+        scansAMonth: 780,
+      },
+      {
+        teamName: "Team 8",
+        date: "2021-08-13",
+        scansAMonth: 276,
+      },
+      {
+        teamName: "Team 8",
+        date: "2021-07-13",
+        scansAMonth: 758,
+      },
+    ],
+  },
+  {
+    teamName: "Team 9",
+    totalScans: 2440,
+    reports: [
+      {
+        teamName: "Team 9",
+        date: "2021-12-13",
+        scansAMonth: 689,
+      },
+      {
+        teamName: "Team 9",
+        date: "2021-11-13",
+        scansAMonth: 325,
+      },
+      {
+        teamName: "Team 9",
+        date: "2021-10-13",
+        scansAMonth: 5,
+      },
+      {
+        teamName: "Team 9",
+        date: "2021-09-13",
+        scansAMonth: 615,
+      },
+      {
+        teamName: "Team 9",
+        date: "2021-08-13",
+        scansAMonth: 324,
+      },
+      {
+        teamName: "Team 9",
+        date: "2021-07-13",
+        scansAMonth: 482,
+      },
+    ],
+  },
+  {
+    teamName: "Team 10",
+    totalScans: 2532,
+    reports: [
+      {
+        teamName: "Team 10",
+        date: "2021-12-13",
+        scansAMonth: 371,
+      },
+      {
+        teamName: "Team 10",
+        date: "2021-11-13",
+        scansAMonth: 57,
+      },
+      {
+        teamName: "Team 10",
+        date: "2021-10-13",
+        scansAMonth: 403,
+      },
+      {
+        teamName: "Team 10",
+        date: "2021-09-13",
+        scansAMonth: 949,
+      },
+      {
+        teamName: "Team 10",
+        date: "2021-08-13",
+        scansAMonth: 596,
+      },
+      {
+        teamName: "Team 10",
+        date: "2021-07-13",
+        scansAMonth: 156,
+      },
+    ],
+  },
+];
+
+export default DUMMY_DATA;
