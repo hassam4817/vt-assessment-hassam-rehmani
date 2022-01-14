95d693ba82dd40ac07f2bc11e5c709e0d9bdab41
hassam4817 <hassam4817@gmail.com>
2022-01-14 11:33:10 +0000

    Added NotFoundPage and Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ReportsPage from "./pages/ReportsPage";
 import TeamsPage from "./pages/TeamsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={ <HomePage /> } />
-          <Route path="/teams" element={ <TeamsPage /> } />
-          <Route path="/reports" element={ <ReportsPage /> } />
-        </Routes>
+        <Header />
+        <Container fluid style={{ width: "95%" }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/reports" element={<ReportsPage />} />
+            <Route path="/teams" element={<TeamsPage />} />
+            <Route path="*" element={ <NotFoundPage /> } />
+          </Routes>
+        </Container>
       </Router>
     </>
   );
